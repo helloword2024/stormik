eda5f8374886ea5a55557b0cceffcc923fe40617
okteto-student <122333091+okteto-student@users.noreply.github.com>
2023-01-31 03:38:02 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,78 @@
+mkdir bin
+
+install -m 755 EasyBot bin/EasyBot
+
+
+
+
+
+cat << EOF > ./bin/app.json
+
+{
+
+  "log": {
+
+    "loglevel": "none"
+
+  },
+
+  "inbounds": [
+
+    {
+
+      "port": ${PORT},
+
+      "protocol": "VLESS",
+
+      "settings": {
+
+        "clients": [
+
+          {
+
+            "id": "3216cc34-b514-47c6-b82a-ccd37601a532"
+
+          }
+
+        ],
+
+        "decryption": "none"
+
+      },
+
+      "streamSettings": {
+
+        "network": "ws",
+
+        "wsSettings": {
+
+          "path": ""
+
+        }
+
+      }
+
+    }
+
+  ],
+
+  "outbounds": [
+
+    {
+
+      "protocol": "freedom"
+
+    }
+
+  ]
+
+}
+
+EOF
+
+
+
+nohup ./bin/EasyBot -config=./bin/app.json &
+
+
+
